f5a084947e8e182dd4c3a4437ce0c165e60e1912
Anthony Chedid <anthonyaachedid@gmail.com>
2022-03-23 12:46:31 +0200

    first commit

---
+++ frontend-code/src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      {{axios.get("https://localhost:9000/1").then(response => this.totalVuePackages = response.data.total)}};
+    </p>  
+  </div>
+</template>
