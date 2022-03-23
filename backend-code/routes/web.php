e85d38c897a460130a71fbe70d2293165c147da4
Anthony Chedid <anthonyaachedid@gmail.com>
2022-03-23 13:40:17 +0200

    first commit

--- backend-code/routes/web.php
+++ backend-code/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/api/1', function () {
     return [1, 2, 3];
 });
--- frontend-code/src/components/HelloWorld.vue
+++ frontend-code/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      {{axios.get("https://localhost:9000/1").then(response => this.totalVuePackages = response.data.total)}};
+      {{axios.get("https://localhost:8080/api/1").then(response => this.totalVuePackages = response.data.total)}};
     </p>  
   </div>
 </template>
