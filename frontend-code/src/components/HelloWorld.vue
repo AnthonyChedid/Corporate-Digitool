1e5c6552c96e5dbb96a5c70e9fe6616a4da9b0dd
Anthony Chedid <anthonyaachedid@gmail.com>
2022-03-24 18:07:26 +0200

    adding phpmyadmin to docker

--- frontend-code/src/components/HelloWorld.vue
+++ frontend-code/src/components/HelloWorld.vue
@@ -2,7 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      {{axios.get("https://localhost:8080/api/1").then(response => this.totalVuePackages = response.data.total)}};
+      
+    
     </p>  
   </div>
 </template>
