958f170b0ff000d2bf49fd0757e7f98e28f6bdff
Caline Doumit <calinedoumit@gmail.com>
2022-05-04 23:14:33 +0300

    Assign a team

--- backend-code/app/Http/Controllers/TeamUserController.php
+++ backend-code/app/Http/Controllers/TeamUserController.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 class TeamUserController extends VoyagerBaseController
 {
     public function addTeamMember(Request $request){
-
+        team_user::create([
+            'team_id'=>$request->team;
+            'user_id'=>$request->user;
+        ]);
     }
 }
